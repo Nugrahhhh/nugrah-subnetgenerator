c047fe570f0ad0c2a65ca1aeaa07abb203c7c344
Nugrahhhh <139343733+Nugrahhhh@users.noreply.github.com>
2024-03-10 16:28:10 +0700

    Upload all files

---
+++ script.js
@@ -0,0 +1,12 @@
+function generateRandomIPAndNetmask() {
+    var ip = "";
+    for (var i = 0; i < 4; i++) {
+        ip += Math.floor(Math.random() * 256);
+        if (i < 3) ip += ".";
+    }
+
+    var netmask = "/"+(Math.floor(Math.random() * 9) + 24);
+
+    var result = ip + " " + netmask;
+    document.getElementById('result').innerHTML = "<p>Generated IP Address with Netmask: " + result + "</p>";
+}
